16e313229de246ee560fc3b6fd64d9d678d68cfa
Artin Kiany <artinkiany59@gmail.com>
2026-02-03 21:20:21 -0500

    SPEED PLEASSEEE

--- rtl/linear_diffusion_layer.sv
+++ rtl/linear_diffusion_layer.sv
@@ -19,16 +19,16 @@ module linear_diffusion_layer(
     assign s3 = state_array_i[3];
     assign s4 = state_array_i[4];
 
-    
+
     logic [63:0] s0_d, s1_d, s2_d, s3_d, s4_d;
-    
+
     always_comb begin
         // Rotate-right implemented via logical shifts
-        s0_d = s0 ^ ((s0 >> 19) | (s0 << (WORD_WIDTH - 19))) ^ ((s0 >> 28) | (s0 << (WORD_WIDTH - 28)));
-        s1_d = s1 ^ ((s1 >> 61) | (s1 << (WORD_WIDTH - 61))) ^ ((s1 >> 39) | (s1 << ( WORD_WIDTH- 39)));
-        s2_d = s2 ^ ((s2 >> 1)  | (s2 << (WORD_WIDTH - 1)))  ^ ((s2 >> 6)  | (s2 << ( WORD_WIDTH- 6)));
-        s3_d = s3 ^ ((s3 >> 10) | (s3 << (WORD_WIDTH - 10))) ^ ((s3 >> 17) | (s3 << ( WORD_WIDTH- 17)));
-        s4_d = s4 ^ ((s4 >> 7)  | (s4 << (WORD_WIDTH - 7)))  ^ ((s4 >> 41) | (s4 << ( WORD_WIDTH- 41)));
+        s0_d= s0 ^ ((s0 >> 19) | (s0 << (WORD_WIDTH - 19))) ^ ((s0 >> 28) | (s0 << (WORD_WIDTH - 28)));
+        s1_d= s1 ^ ((s1 >> 61) | (s1 << (WORD_WIDTH - 61))) ^ ((s1 >> 39) | (s1 << (WORD_WIDTH- 39)));
+        s2_d= s2 ^ ((s2 >> 1)  | (s2 << (WORD_WIDTH - 1)))  ^ ((s2 >> 6)  | (s2 << (WORD_WIDTH- 6)));
+        s3_d= s3 ^ ((s3 >> 10) | (s3 << (WORD_WIDTH - 10))) ^ ((s3 >> 17) | (s3 << (WORD_WIDTH- 17)));
+        s4_d= s4 ^ ((s4 >> 7)  | (s4 << (WORD_WIDTH - 7)))  ^ ((s4 >> 41) | (s4 << (WORD_WIDTH- 41)));
     end
 
     // Preserve Ascon word ordering: index 0 = S0, index 4 = S4
@@ -37,7 +37,7 @@ module linear_diffusion_layer(
     assign state_array_o[2] = s2_d;
     assign state_array_o[3] = s3_d;
     assign state_array_o[4] = s4_d;
-    
+
 
 
 endmodule
