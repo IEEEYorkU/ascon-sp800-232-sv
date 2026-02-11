5b83179a5d5df899a127a0174dea5922d19483c7
Arthur Sabadini Nascimento <144047290+ArthurSabadini@users.noreply.github.com>
2026-02-11 06:08:08 -0500

    Update rtl/substitution_layer.sv
    Co-authored-by: Kiet Le <ktlbusiness11@gmail.com>

--- rtl/substitution_layer.sv
+++ rtl/substitution_layer.sv
@@ -1,6 +1,6 @@
 /*
  * Module Name: substitution_layer
- * Author(s): Arthur Sabadini, Kevin Duong (add your name as you make changes)
+ * Author(s): Arthur Sabadini, Kevin Duong
  * Description: The substitution layer applies a 5-bit nonlinear S-box in bit-sliced form across the entire state.
  * Ref: NIST SP 800-232
  */
