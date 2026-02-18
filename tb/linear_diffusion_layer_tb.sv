731f2517bf2663f89b5eb772cd571f3dceac1b74
Tirth Patel <tirth.patel1721@gmail.com>
2026-02-18 15:13:08 -0500

    resolved a few style issues

--- tb/linear_diffusion_layer_tb.sv
+++ tb/linear_diffusion_layer_tb.sv
@@ -29,11 +29,20 @@ module tb_linear_diffusion_layer;
         input  ascon_state_t in_state,
         output ascon_state_t exp_state
     );
-        int r_a [0:4]; //rotation a
-        int r_b [0:4]; //rotation b
-        
-        r_a[0] = 19; r_a[1] = 61; r_a[2] = 1; r_a[3] = 10; r_a[4] = 7;  // word rotation 1 from ascon pdf
-        r_b[0] = 28; r_b[1] = 39; r_b[2] = 6; r_b[3] = 17; r_b[4] = 41; // word rotation 2 from ascon pdf
+        int r_a [5]; //rotation a
+        int r_b [5]; //rotation b
+
+        r_a[0] = 19;
+        r_a[1] = 61;
+        r_a[2] = 1;
+        r_a[3] = 10;
+        r_a[4] = 7;  // word rotation 1 from ascon pdf
+
+        r_b[0] = 28;
+        r_b[1] = 39;
+        r_b[2] = 6;
+        r_b[3] = 17;
+        r_b[4] = 41; // word rotation 2 from ascon pdf
 
         for (int i = 0; i < 5; i++) begin
             exp_state[i] = in_state[i] ^ ror64(in_state[i], r_a[i]) ^ ror64(in_state[i], r_b[i]);
@@ -45,8 +54,10 @@ module tb_linear_diffusion_layer;
     task automatic fail_mismatch(input int tid, input ascon_state_t expected_val);
         $display("\n[FAIL] test_id=%0d", tid);
         for (int i = 0; i < 5; i++) begin
-            $display("  Word %0d | IN: %h | DUT: %h | EXP: %h", 
-                     i, state_i[i], state_o[i], expected_val[i]);
+            $display(
+                "  Word %0d | IN: %h | DUT: %h | EXP: %h",
+                i, state_i[i], state_o[i], expected_val[i]
+            );
         end
         $finish;
     endtask
@@ -74,10 +85,10 @@ module tb_linear_diffusion_layer;
         #1;
 
         compute_expected(state_i, exp);
-        
+
         mismatch = 0;
         for (int i = 0; i < 5; i++) if (state_o[i] !== exp[i]) mismatch = 1;
-        
+
         if (mismatch) fail_mismatch(test_id, exp);
         $display("Test 1: All Zeros PASSED wohoo.");
         test_id++;
@@ -86,13 +97,13 @@ module tb_linear_diffusion_layer;
 
         for (int i = 0; i < 5; i++) begin
             for (int w = 0; w < 5; w++) state_i[w] = 64'h0;
-            state_i[i] = 64'h1; 
+            state_i[i] = 64'h1;
             #1;
             compute_expected(state_i, exp);
-            
+
             mismatch = 0;
             for (int k = 0; k < 5; k++) if (state_o[k] !== exp[k]) mismatch = 1;
-            
+
             if (mismatch) fail_mismatch(test_id, exp);
             $display("Test 2: Word %0d Bit 1 Test Passed wohoo.", i);
             test_id++;
@@ -104,7 +115,6 @@ module tb_linear_diffusion_layer;
 
         for (int t = 0; t < num_random_tests; t++) begin
             for (int r = 0; r < 5; r++) state_i[r] = rand_word();
-            
             #1;
             compute_expected(state_i, exp);
 
