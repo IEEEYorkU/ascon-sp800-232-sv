e0c187ebb8d7482c977dd3577ce73873a8016212
Artin Kiany <artinkiany59@gmail.com>
2026-02-18 18:49:22 -0500

    Added ascon core tb to make file

--- rtl/ascon_core.sv
+++ rtl/ascon_core.sv
@@ -19,7 +19,7 @@ module ascon_core (
     // Read/Write Word Address
     input   logic [2:0]     word_sel_i,
 
-    // Data I/SO Control
+    // Data I/O Control
     input   ascon_word_t    data_i,
     input   logic           write_en_i,
     input   logic           xor_en_i,
@@ -38,13 +38,13 @@ module ascon_core (
 		STATE_PERM
     } state_t;
     state_t state = STATE_IDLE, next_state;
-	
+
 	rnd_t rnd_cnt;
 	ascon_state_t state_array = 320'd0;
-	
+
 	// Permutation Layers Output
 	ascon_state_t addition_state_array_o, substitution_state_array_o, diffusion_state_array_o;
-	
+
 	// Permutation Layers Instances
 	constant_addition_layer const_add(
         .rnd_i(rnd_cnt),
@@ -59,7 +59,7 @@ module ascon_core (
         .state_array_i(substitution_state_array_o),
         .state_array_o(diffusion_state_array_o)
     );
-	
+
     // FSM Control Process 1: State Register (Sequential)
     // ----------------------------------------------------------
     always_ff @(posedge clk or posedge rst) begin
@@ -69,12 +69,12 @@ module ascon_core (
             state <= next_state;
         end
     end
-    
+
 	// FSM Control Process 2: Next State Decoder (Combinational)
     // ----------------------------------------------------------
 	always_comb begin
         next_state = state;
-	
+
 	    case(state)
             STATE_IDLE: begin
                 if(start_perm_i) begin
@@ -83,11 +83,11 @@ module ascon_core (
                     next_state = STATE_IDLE;
                 end
             end
-            
+
             STATE_INIT: begin
                 next_state = STATE_PERM;
             end
-            
+
             STATE_PERM: begin
                 if(rnd_cnt > 0) begin
                     next_state = STATE_PERM;
@@ -97,20 +97,20 @@ module ascon_core (
             end
         endcase
 	end
-    
+
     // FSM Control Process 3: Action Decoder (Combinational)
     // ----------------------------------------------------------
     always_comb begin
         ready_o = 1'd0;
         data_o = state_array[word_sel_i];
-        
+
         case(state)
             STATE_IDLE: begin
                 ready_o = 1'd1;
             end
         endcase
     end
-	
+
     // FSM Control Process 4: Action Logic (Sequential)
     // ----------------------------------------------------------
     always_ff @(posedge clk or posedge rst) begin
@@ -128,7 +128,7 @@ module ascon_core (
                         end
                     end
                 end
-				  
+
                 STATE_INIT: begin
                     // Set counter to N-1, to have exactly N rounds.
                     if(round_config_i) begin
