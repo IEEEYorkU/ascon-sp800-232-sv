5607dfb7b1fbdb70698556098385caf258fee9f4
Kiet Le <ktlbusiness11@gmail.com>
2026-02-19 00:18:55 -0500

    GHA: import 2

--- rtl/substitution_layer.sv
+++ rtl/substitution_layer.sv
@@ -6,18 +6,16 @@
  * Ref: NIST SP 800-232
  */
 
-import ascon_pkg::*;
-
 module substitution_layer (
-    input   ascon_state_t   state_array_i,
-    output  ascon_state_t   state_array_o
+    input   ascon_pkg::ascon_state_t   state_array_i,
+    output  ascon_pkg::ascon_state_t   state_array_o
 );
 
     // ----------------------------------------------------------------------
     // S-box Look-Up Table (LUT)
     // ----------------------------------------------------------------------
     // See NIST SP 800-232 Section 3.3 Table 6
-    localparam logic [NUM_WORDS-1:0] Sbox [32] = '{
+    localparam logic [ascon_pkg::NUM_WORDS-1:0] Sbox [32] = '{
         5'h4, 5'hb, 5'h1f, 5'h14, 5'h1a, 5'h15, 5'h9, 5'h2,
         5'h1b, 5'h5, 5'h8, 5'h12, 5'h1d, 5'h3, 5'h6, 5'h1c,
         5'h1e, 5'h13, 5'h7, 5'he, 5'h0, 5'hd, 5'h11, 5'h18,
@@ -29,7 +27,7 @@ module substitution_layer (
     // ----------------------------------------------------------------------
     genvar j;
     generate
-        for (j = 0; j < WORD_WIDTH; j++) begin : gen_sbox_loop
+        for (j = 0; j < ascon_pkg::WORD_WIDTH; j++) begin : gen_sbox_loop
             // Note that concatenation is needed here. As the
             // input of the Sbox LUT must be a number.
             assign {
